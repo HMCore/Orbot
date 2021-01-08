fcaa8377c1002719ae63835ce63894070d60c73f
Wieland Schöbl <wulkanat@gmail.com>
2021-01-08 15:41:37 +0100

    Move connection failed message to Admin

--- src/main/kotlin/de/wulkanat/DiscordRpc.kt
+++ src/main/kotlin/de/wulkanat/DiscordRpc.kt
@@ -7,9 +7,11 @@ object DiscordRpc {
     var jda: JDA? = null
 
     fun updatePresence(available: Boolean) {
-        jda ?: return
+        // jda ?: return
 
-        jda!!.presence.activity = Activity.watching(if (available) Admin.message else Admin.offlineMessage)
-        jda!!.presence.isIdle = !available
+        // jda!!.presence.activity = Activity.watching(if (available) Admin.message else Admin.offlineMessage)
+        // jda!!.presence.isIdle = !available
+        // noop
+        if (available) Admin.println("Back online") else Admin.error("Gone offline", "Can't reach Hytale server")
     }
 }
